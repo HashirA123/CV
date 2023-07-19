a8b764b158fca58fba902b38c398afe505713e39
Hashir Ahmed <59901350+HashirA123@users.noreply.github.com>
2023-07-19 15:29:02 -0600

    Updated resume and about me

--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -19,12 +19,19 @@ function Experience() {
         </div>
         <div  className="right-content">
         <h2>I'm Hashir</h2>
-        <p>I am a software engineering student at the University of Calgary, currently in my final year. 
+        <p>
+        I am passionate about applying my skills and knowledge to solve real-world problems using machine learning and data analysis.
+
         Through my academic career, I have built a strong set of skills in various programming languages, most notably Java, C/C++, 
         and many useful programming paradigms. I also have valuable experience when it comes to version control through GIT, and web 
         development with JavaScript, PHP, and MySQL. Along with the academic skills, I am also building strong interpersonal and 
         communication skills through volunteering in places like the Students Union at U of C. Though there may be skills that 
-        I have not yet learned or perfected, I am always up to new challenges, always seeking to better myself as a person and professional. </p>
+        I have not yet learned or perfected, I am always up to new challenges, always seeking to better myself as a person and professional.
+        
+        Currently, I am an Associate Machine Learning Developer at AltaML, where I work with a team of experts to develop and deploy ML 
+        solutions for various clients and industries. I am always eager to learn new technologies and tools, and I am looking for new grad 
+        opportunities that will challenge me and help me grow as a developer. Please feel free to reach out!
+         </p>
 
         </div>
       </div>
