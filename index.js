6b955db75de9a868deae31f4089df3fbb4163ed2
Hashir Ahmed <59901350+HashirA123@users.noreply.github.com>
2023-11-17 20:37:21 -0700

    Updates

--- index.js
+++ index.js
@@ -4,37 +4,26 @@
 //   $.holdReady(false);
 // }, 2200);
 
-jQuery(function () {
-  // loading animation
-  $("#loading_start").css("display", "none");
-
-  // typing animation
-  var typed = new Typed(".typing", {
-    strings: ["Engineer", "Developer", "Versatile", "Gamer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true,
-  });
-
-  // carousel animation
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplayTime: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
-});
+// jQuery(function () {
+//   // carousel animation
+//   // $(".carousel").owlCarousel({
+//   //   margin: 20,
+//   //   loop: true,
+//   //   autoplayTime: 2000,
+//   //   autoplayHoverPause: true,
+//   //   responsive: {
+//   //     0: {
+//   //       items: 1,
+//   //       nav: false,
+//   //     },
+//   //     600: {
+//   //       items: 2,
+//   //       nav: false,
+//   //     },
+//   //     1000: {
+//   //       items: 3,
+//   //       nav: false,
+//   //     },
+//   //   },
+//   // });
+// });
