450cc4ec7b90f68c8cd13b777621b746a7d57e4e
Hashir Ahmed <59901350+HashirA123@users.noreply.github.com>
2023-12-22 21:55:01 -0700

    updated linkedIn link

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import "../styles/Contact.css"
-import ContactBox from '../components/ContactBox'
+import React from "react";
+import "../styles/Contact.css";
+import ContactBox from "../components/ContactBox";
 
 function Contact() {
   return (
-    <div id="Contact" className='contact'>
+    <div id="Contact" className="contact">
       <h1>Contact Me</h1>
       <div className="boxes">
-        <ContactBox animation="fade-right" class= "fas fa-envelope" name="Email" link="mailto:hashirahmed204@gmail.com"/>
-        <ContactBox animation="fade-up" class= "fab fa-github" name="Github" link="https://github.com/HashirA123"/>
-        <ContactBox animation="fade-left" class= "fab fa-linkedin" name="Linkedin" link="https://www.linkedin.com/in/hashir-ahmed-2531b71b6/"/>
+        <ContactBox
+          animation="fade-right"
+          class="fas fa-envelope"
+          name="Email"
+          link="mailto:hashirahmed204@gmail.com"
+        />
+        <ContactBox
+          animation="fade-up"
+          class="fab fa-github"
+          name="Github"
+          link="https://github.com/HashirA123"
+        />
+        <ContactBox
+          animation="fade-left"
+          class="fab fa-linkedin"
+          name="Linkedin"
+          link="https://www.linkedin.com/in/hashirahmed204/"
+        />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
