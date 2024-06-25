fcfae2290efbf84e0267755502dbfd189cd5307e
Hashir Ahmed <59901350+HashirA123@users.noreply.github.com>
2024-06-24 19:59:16 -0600

    Dynamically update all rights reserved date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import "../styles/Footer.css"
+import React from "react";
+import "../styles/Footer.css";
 
 function Footer() {
+  var date = new Date();
   return (
-    <div className='footer'>
-        <div className='author'>
+    <div className="footer">
+      <div className="author">
         <p>Created by Hashir Ahmed</p>
-        </div>
-        <div className=''>
-        <p className='copyright'> <i className='far fa-copyright'></i> 2023 All rights reserved</p>
-        </div>
+      </div>
+      <div className="">
+        <p className="copyright">
+          <i className="far fa-copyright"></i> {date.getFullYear()} All rights
+          reserved
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
